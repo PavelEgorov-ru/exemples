12382bd298f570c73a38c73876de1e88f5af0188
PavelEgorov-ru <pkotofffff@yandex.ru>
2024-10-31 23:28:40 +0500

    пример полного ветвления

---
+++ pascal/exemp/if_else.pas
@@ -0,0 +1,11 @@
+program if_else;
+var
+    x: integer;
+begin
+    read(x);
+    if x > 0 then
+        writeln('число положительное')
+    else 
+        writeln('число отрицательное');
+    writeln('конец программы')
+end.
