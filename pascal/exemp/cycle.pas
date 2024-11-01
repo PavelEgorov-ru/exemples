1b977b08dbb14e595ff69df4644d7b2bbffe0478
PavelEgorov-ru <pkotofffff@yandex.ru>
2024-11-01 22:34:16 +0500

    пример цикла с предусловием и логичексим выражением

--- pascal/exemp/cycle.pas
+++ pascal/exemp/cycle.pas
@@ -1,15 +1,18 @@
 program cycle;
 var 
     year: integer;
+    positive: boolean;
 
 begin
     writeln('Введите свой год рождения');
     readln(year);
-    while (year < 1900) or (year > 2020) do
+    positive := (year < 1900) or (year > 2020);
+    while positive do
     begin
         writeln(year, ' не может быть готом рождения');
         writeln('Введите свой год рождения');
-        readln(year)
+        readln(year);
+        positive := (year < 1900) or (year > 2020)
     end;
     writeln('Спасибо !')
 end.
