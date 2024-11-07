256c1d41d4a04e4573379b84ae17a967f9a5a1fa
PavelEgorov-ru <pkotofffff@yandex.ru>
2024-11-07 22:35:48 +0500

    возможность выбрать любой символ для рисования алмаза

--- pascal/diamond/diamond_2.pas
+++ pascal/diamond/diamond_2.pas
@@ -1,11 +1,14 @@
 program diamond;
 var
     half_height, line: integer;
+    ch: char;
 
-procedure DialogUser(var h: integer);
+procedure DialogUser(var h: integer; var c: char);
 var
     height: integer;
+    symbol: char;
     phrase: string = 'введите высоту алмаза, обязательно нечетное число';
+    phrase_2: string = 'введите символ, которым заполнится алмаз';
 
 function HalfHeight(a: integer): integer;
 begin
@@ -17,7 +20,10 @@ begin
         writeln(phrase);
         readln(height)
     until (height > 0) and (height mod 2 = 1);
-    h := HalfHeight(height)
+    h := HalfHeight(height);
+    writeln(phrase_2);
+    readln(symbol);
+    c := symbol
 end;
 
 
@@ -29,26 +35,26 @@ begin
         write(' ')
 end;
 
-procedure PrintLineOfDiamond(n, k: integer);
+procedure PrintLineOfDiamond(n, k: integer; c: char);
 begin
     PrintSpace(n + 1 - k);
-    write('*');
+    write(c);
     if k > 1 then
     begin
         PrintSpace(2 * k - 3);
-        write('*')
+        write(c)
     end;
     writeln;
 end;
 
 begin
     { ввод числа, пока пользователь не введет его как надо }
-    DialogUser(half_height);
+    DialogUser(half_height, ch);
     { печать верхней фигуры }
     for line := 1 to half_height + 1 - line do 
-        PrintLineOfDiamond(half_height, line);
+        PrintLineOfDiamond(half_height, line, ch);
     { печать нижней части }
     for line := half_height downto 1 do
-        PrintLineOfDiamond(half_height, line)
+        PrintLineOfDiamond(half_height, line, ch)
 end.
 
