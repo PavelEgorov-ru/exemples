7df0c08bf95fe8a183831fcf3ea229ee19f8a57a
PavelEgorov-ru <pkotofffff@yandex.ru>
2024-12-07 12:30:13 +0500

    заполнение массивов и вывод на экран

--- pascal/read_str/read_str.pas
+++ pascal/read_str/read_str.pas
@@ -6,5 +6,38 @@ const
 type
     CheckCharsArray = array [1..MaxChars] of word;
     PrintCharsArray = array [1..255] of word;
+
+var
+    i: integer;
+    {symbol: char;}
+    check_array: CheckCharsArray;
+    print_array: PrintCharsArray;
+
+
+procedure PrintPrintArr (var arr: PrintCharsArray; max_index: word);
+var
+    i: integer;
 begin
+    for i := 1 to max_index do
+        write(arr[1], ' ');
+    writeln
+end;
+
+procedure PrintCheckArr (var arr: CheckCharsArray; max_index: word);
+var
+    i: integer;
+begin
+    for i := 1 to max_index do
+        write(arr[1], ' ');
+    writeln
+end;
+
+begin
+    for i := 1 to MaxChars do
+        check_array[i] := 0;
+    for i := 1 to 255 do
+        print_array[i] := 0;
+    PrintCheckArr(check_array, MaxChars);
+    writeln;
+    PrintPrintArr(print_array, 255)
 end.
