3943595a249e6fbeea9e832023fe986d843479f4
PavelEgorov-ru <pkotofffff@yandex.ru>
2025-01-26 22:06:37 +0500

    промежуточный результат

--- pascal/read_str/read_str.pas
+++ pascal/read_str/read_str.pas
@@ -9,11 +9,11 @@ type
 
 var
     i: integer;
-    {symbol: char;}
+    symbol: char;
     check_array: CheckCharsArray;
     print_array: PrintCharsArray;
 
-
+{$IFDEF DEBUG}
 procedure PrintPrintArr (var arr: PrintCharsArray; max_index: word);
 var
     i: integer;
@@ -31,13 +31,21 @@ begin
         write(arr[1], ' ');
     writeln
 end;
+{$ENDIF}
 
 begin
     for i := 1 to MaxChars do
         check_array[i] := false;
     for i := 1 to 255 do
         print_array[i] := 0;
+{$IFDEF DEBUG}
+    writeln('test');
+{$ENDIF}
+        
+{$IFDEF DEBUG}
     PrintCheckArr(check_array, MaxChars);
     writeln;
     PrintPrintArr(print_array, 255)
+{$ENDIF}
+    
 end.
